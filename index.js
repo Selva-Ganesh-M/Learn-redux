66bef30d1697aa2a7b1e6ac21b8c9a4546b02b3d
Selva Ganesh M <selvadev2k@gmail.com>
2022-11-19 16:14:09 +0530

    basic syntax of the redux store creation

---
+++ index.js
@@ -0,0 +1,56 @@
+const redux = require("redux");
+const createStore = redux.legacy_createStore;
+
+const initialState = {
+  loading: false,
+  data: [],
+  error: "",
+};
+
+const FETCH_USERS_REQUESTED = "FETCH_USERS_REQUESTED";
+const FETCH_USERS_SUCCEEDED = "FETCH_USERS_SUCCEEDED";
+const FETCH_USERS_FAILED = "FETCH_USERS_FAILED";
+
+const fetchUsersRequest = () => {
+  return {
+    type: FETCH_USERS_REQUESTED,
+  };
+};
+
+const fetchUsersSuccess = (users) => {
+  return {
+    type: FETCH_USERS_SUCCEEDED,
+    payload: users,
+  };
+};
+
+const fetchUsersFailed = (error) => {
+  return {
+    type: FETCH_USERS_FAILED,
+    payload: error,
+  };
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_USERS_REQUESTED:
+      return {
+        ...state,
+        loading: true,
+      };
+    case FETCH_USERS_SUCCEEDED:
+      return {
+        loading: false,
+        data: action.payload,
+        error: "",
+      };
+    case FETCH_USERS_FAILED:
+      return {
+        loading: false,
+        data: [],
+        error: action.payload,
+      };
+  }
+};
+
+const store = createStore(reducer);
